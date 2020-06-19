7ea9405224695a2216046a60a27d02df7254beb1
gaganverma <gagan.verma@engineer.ai>
2020-06-19 13:06:55 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -186,7 +186,7 @@ export class AppComponent implements AfterViewInit {
         function msgLoop (i) {
           $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
             i=i+1;
-            $("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+            $("p:nth-child("+i+")").fadeIn('slow').delay(3100);
             if(i==50){
               $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
                 $('.cake').fadeIn('fast');
