97d95b1edb4f755e76436cbd402f150f3fb98df7
gagan96verma <gagan96verma@gmail.com>
2021-05-01 12:33:18 +0530

    kk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -35,7 +35,7 @@ export class AppComponent implements AfterViewInit {
         $('#bulb_green').addClass('bulb-glow-green');
         $('#bulb_pink').addClass('bulb-glow-pink');
         $('#bulb_orange').addClass('bulb-glow-orange');
-        $('body').addClass('peach');
+        // $('body').addClass('peach');
         $(this).fadeOut('slow').delay(5000).promise().done(function(){
           $('#play').fadeIn('slow');
         });
@@ -48,8 +48,8 @@ export class AppComponent implements AfterViewInit {
         $('#bulb_green').addClass('bulb-glow-green-after');
         $('#bulb_pink').addClass('bulb-glow-pink-after');
         $('#bulb_orange').addClass('bulb-glow-orange-after');
-        $('body').css('backgroud-color','#FFF');
-        $('body').addClass('peach-after');
+        $('body').css('backgroud-color','black');
+        // $('body').addClass('peach-after');
         $(this).fadeOut('slow').delay(6000).promise().done(function(){
           $('#bannar_coming').fadeIn('slow');
         });
@@ -169,8 +169,8 @@ export class AppComponent implements AfterViewInit {
         $('#b66').animate({top:240, left: vw+150},500);
         $('#b77').animate({top:240, left: vw+250},500);
         $('.balloons').css('opacity','0.9');
-        $('.balloons h2').fadeIn(3000);
-        $(this).fadeOut('slow').delay(3000).promise().done(function(){
+        $('.balloons h2').fadeIn(4000);
+        $(this).fadeOut('slow').delay(4000).promise().done(function(){
           $('#story').fadeIn('slow');
         });
       });
@@ -184,9 +184,9 @@ export class AppComponent implements AfterViewInit {
         var i;
 
         function msgLoop (i) {
-          $("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+          $("p:nth-child("+i+")").fadeOut('slow').delay(2000).promise().done(function(){
             i=i+1;
-            $("p:nth-child("+i+")").fadeIn('slow').delay(3100);
+            $("p:nth-child("+i+")").fadeIn('slow').delay(3500);
             if(i==50){
               $("p:nth-child(49)").fadeOut('slow').promise().done(function () {
                 $('.cake').fadeIn('fast');
